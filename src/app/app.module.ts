78e6d5db56996e8b62d6ee1b4121efcae1abd44c
ruxandra <a.ruxandra@yahoo.com>
2017-11-05 07:29:46 +0200

    commit submodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/Common';
 
 import { AppComponent } from './app.component';
+import { ListComponent } from './list/list.component';
+import { MovieslistService } from './services/movieslist/movieslist.service';
+import { BoldDirective } from './directives/bold/bold.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    BoldDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [MovieslistService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
